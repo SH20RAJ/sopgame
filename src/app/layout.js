542ef6302ccfb6d1b07dbae315442f63183f161b
sh20raj <shaswatraj3@gmail.com>
2024-08-11 02:17:58 +0530

    shadcn components

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828915420581549"
+     crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
